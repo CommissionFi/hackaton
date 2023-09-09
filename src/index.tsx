5a642552e90efa9ed1314ca781f979acff39e8e9
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-09-09 17:51:40 +0000

    Dependencies Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import User from './User';
@@ -12,17 +12,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route path="/u/:input" component={User} />
-        <Route path="/" exact component={App} />
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/u/:input" Component={User} />
+        <Route path="/" Component={App} />
+      </Routes>
+    </BrowserRouter>
     <AppFooter />
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
