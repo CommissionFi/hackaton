4b0c84b70b6c4feeec8021ba82879bf4cc428dfa
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-09-09 06:48:30 -0700

    Header .tsx implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const redirectToDashboard = () => {
+    // Replace the URL with the actual dashboard URL
+    window.location.href = '/dashboard';
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to CommissionFi</h1>
+        <div className="Logo">
+          CommissionFi
+        </div>
+        <div className="dashboardButton">
+          <button onClick={redirectToDashboard} className="dashboard-button">
+            Go to Dashboard
+          </button>
+        </div>
+      </header>
+      <h1>Welcome to CommissionFi</h1>
         <h2>Empowering artists and buyers to create, collaborate, and transact safely in the world of commissions</h2>
         <p>
           Integer id venenatis arcu. Morbi tristique, odio vel euismod
@@ -16,7 +30,6 @@ function App() {
           Vivamus fermentum, risus sit amet malesuada rhoncus, ligula leo
           suscipit dolor, non vulputate libero mauris nec justo.
         </p>
-      </header>
     </div>
   );
 }
