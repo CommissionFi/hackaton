d99a2a22d3c70abe7628abdbd61d542e6fc74049
DanIsNearby.eth <91475837+DanIsNearby@users.noreply.github.com>
2023-09-09 06:37:08 -0700

    Lorem Ipsum

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const redirectToDashboard = () => {
-    // Replace the URL with the actual dashboard URL
-    window.location.href = '/dashboard';
-  };
-
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to CommissionFi</h1>
+        <h2>Empowering artists and buyers to create, collaborate, and transact safely in the world of commissions</h2>
+        <p>
+          Integer id venenatis arcu. Morbi tristique, odio vel euismod
+          ullamcorper, odio tellus convallis metus, vel pharetra dui nisl a
+          libero.
+        </p>
+        <p>
+          Vivamus fermentum, risus sit amet malesuada rhoncus, ligula leo
+          suscipit dolor, non vulputate libero mauris nec justo.
+        </p>
       </header>
     </div>
   );
